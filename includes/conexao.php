9c9fff5f4889ac161cc52823b73c340793edba6d
Alberson David <81652139+AlbersonDavid@users.noreply.github.com>
2025-03-28 12:36:13 -0300

    Update conexao.php

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,13 +1,14 @@
 <?php
+
 $host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "classificadosicet";
+$user = "projetosufam";
+$pass = "rxA@ompP3l2fe";
+$dbname = "projetosufam_classificadosicet";
 
 try {
   $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-  echo "Erro na conexão: " . $e->getMessage();
+  echo "Erro na conex達o: " . $e->getMessage();
 }
 ?>
